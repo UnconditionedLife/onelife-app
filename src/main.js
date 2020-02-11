64a8d18cf14190cc463ba2d26d128f9b8ba6f90d
Massimo Lavelli <lavelli.massimo@gmail.com>
2020-02-11 14:26:36 +0100

    Added 3box. Creating first onelife spaces.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import CreateBox from './utils/3boxAuth';
 
 Vue.config.productionTip = false;
+
+console.log('in Impact Context Provider');
+console.log('in async call');
+// hard coding only for demo
+const account = '0xaA1B8189DAbC2d55FbABBe3A0C99e4dA6969a2fD';
+CreateBox(account, window.ethereum, ['OneLife']);
 
 new Vue({
   router,
---
+++ src/utils/3boxAuth.js
@@ -0,0 +1,116 @@
+import Box from '3box';
+import uuid from 'uuid/v1';
+
+
+// const profile = await Box.getProfile("0xf43321212844541463dDca19eb6377651445A5AE")
+// console.log(profile)
+
+const ethAccount = window.ethereum.enable();
+console.log(ethAccount);
+
+if (typeof window.ethereum !== 'undefined') {
+  // Ethereum user detected. You can now use the provider.
+  const web3Provider = window.ethereum;
+  console.log(web3Provider);
+}
+
+async function CreateBox(account, provider, spaces) {
+  const box = await Box.openBox(account, provider);
+  // const box = await Box.create(provider)
+  await box.auth(spaces, { account });
+  await box.syncDone;
+  const hasProfile = await box.public.get('name', 'oed');
+  console.log(hasProfile);
+
+  if (!hasProfile) {
+    console.log('setting profile');
+    await box.public.set('name', 'oed');
+  }
+
+  const app = 'OneLife';
+  const space = await box.openSpace(app);
+  // await space.public.remove('impact-list') // used to clear the list
+  // and reset to hard coded values
+
+  const exists = await space.public.get('impact-list');
+  console.log(exists);
+
+  if (!exists) {
+    console.log('setting Impact list');
+    const starterImpacts = [{
+      name: 'OneLife Platform', impact: 'Enabling people to make the impacts they desire.', id: uuid(),
+    }, {
+      name: 'QOLEI', impact: 'Enabling people to legally formalize their impacts.', id: uuid(),
+    }, {
+      name: 'Radical Movement', impact: 'Inspiring people to make the impacts they desire.', id: uuid(),
+    }];
+    console.log(JSON.stringify(starterImpacts));
+    await space.public.set(
+      'impact-list',
+      JSON.stringify(starterImpacts),
+    );
+  }
+
+  // {name: 'Radical Movement', purpose: 'To inspire people to make the impacts
+  // they desire.', id: uuid()},
+  // {name: 'OneLife Network', purpose: 'To enable people to make the impacts
+  // they desire.', id: uuid()},
+  // {name: 'Impact Network', purpose: 'To create an interdependent community
+  // making the impacts they desire.', id: uuid()}
+
+  const impactList = await space.public.get('impact-list');
+  console.log(impactList);
+
+  return JSON.parse(impactList);
+}
+
+export default CreateBox;
+
+
+// below was testing using the Pinata service
+
+/* const pinataApiKey = "ee31a9d6465e600c5287";
+const pinataSecretApiKey = "8846bbfb844ef4d08a6e9737accbff86dd3ccf6d85519351a644672c5ef64d94";
+
+export const testAuthentication = () => {
+    const url = `https://api.pinata.cloud/data/testAuthentication`;
+    return axios
+        .get(url, {
+            headers: {
+                'pinata_api_key': pinataApiKey,
+                'pinata_secret_api_key': pinataSecretApiKey
+            }
+        })
+        .then(function (response) {
+            //handle your response here
+            console.log(response)
+            console.log(response.status)
+        })
+        .catch(function (error) {
+            //handle error here
+            console.log(error)
+        });
+};
+
+export const pinJSONToIPFS = (JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    return axios
+        .post(
+            url,
+            JSONBody,
+            {
+                headers: {
+                    'pinata_api_key': pinataApiKey,
+                    'pinata_secret_api_key': pinataSecretApiKey
+                }
+            }
+        ).then(function (response) {
+            //handle response here
+            console.log(response)
+            console.log(response.status)
+        })
+        .catch(function (error) {
+            //handle error here
+            console.log(error)
+        });
+}; */
